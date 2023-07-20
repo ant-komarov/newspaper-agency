3f933792c6221e1f349532c749afca80564e2aad
Anton Komarov <anton_komarov_qa@ukr.net>
2023-07-20 11:42:58 +0300

    'Create models Topic, Newspaper, Redactor(as AbstractUser) and add models to admin panel'

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tracker.models import Newspaper, Redactor, Topic
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ["title", "topic", "publish_date"]
+    search_fields = ["title"]
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("years_of_experience",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "years_of_experience")}),
+    )
+
+
+admin.site.register(Topic)
